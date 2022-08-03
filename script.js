8bf78ce24103945d74d0d40df25cd6436753427b
SleeplessN <gmlwns7020@naver.com>
2022-08-03 16:37:18 +0900

    feat: Add EventListener on button
    add event on rollbutton and holdbutton

---
+++ script.js
@@ -0,0 +1,10 @@
+const rollButton = document.querySelector(".roll");
+const holdButton = document.querySelector(".hold");
+
+// Roll 버튼 클릭 시 Evnet
+const rollButtonClick = () => {};
+// Hold 버튼 클릭 시 Event
+const holdButtonClick = () => {};
+
+rollButton.addEventListener(click, rollButtonClick);
+holdButton.addEventListener(click, holdButtonClick);
